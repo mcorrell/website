5fda3f538362cd6eb5e35e4b376fe8b5e140f271
Michael Correll <michael.a.correll@gmail.com>
2017-11-30 20:44:48 -0800

    Fixed cv link.

--- cv.php
+++ cv.php
@@ -12,7 +12,7 @@ include('header.php');
 <div id="container">
 	<p>Click <a href="./cv.pdf">here</a> for pdf version.
 	</p>
-	<iframe src="https://docs.google.com/gview?url=https://pages.cs.wisc.edu/~mcorrell/website/cv.pdf&embedded=true" frameborder="0" style="height: 750px; width: 100%;"></iframe>
+	<iframe src="https://docs.google.com/gview?url=http://pages.cs.wisc.edu/~mcorrell/website/cv.pdf&embedded=true" frameborder="0" style="height: 750px; width: 100%;"></iframe>
 </div>
 <?php
 include('footer.php');
