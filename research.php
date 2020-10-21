8ae6baeff908396a48058c42c0d41373788480cb
Michael Correll <michael.a.correll@gmail.com>
2020-10-20 17:28:05 -0700

    Update research.php

--- research.php
+++ research.php
@@ -12,19 +12,32 @@ include('header.php');
 <div id="container">
 <p>I am currently working on a number of research projects dealing with information visualization, with applications in fields like the digital humanities, virology, and psychology. Some example research topics include:</p>
 <ul>
-<li /><p><b>Graphical Perception of Statistics:</b> If we know what someone wants from our data, then we can give them that information directly. In this case a database query might be more suitable than a visualization tool. But the promise of visualization lies in situations where the designer may not know exactly what the viewer wants to know, but the viewer is instead free to explore the dataset to build up understanding by themselves. In order for such designs to work, we as designers need to know how good users are at using <i>visual information</i> to build up big picture <i>statistical information</i> from data.</p>
-<p>Research from pyschology has shown that the visual system is capable of extracting a wide variety of statistical information (average values, variability, extrema) from visual stimuli in the world around us. This research project is an attempt to apply these findings to visualization, to show how we can design displays that rely on these abilities to let viewers build up "big pictures" by themselves.
-<p>Example paper: <a href="http://idl.cs.washington.edu/files/2017-RegressionByEye-CHI.pdf">Regression by Eye: Estimating Trends in Bivariate Visualizations</a></p>
-<li /><p><b>Graphical De-Biasing:</b> Visualizations are not direct links from data to decision. Data must be perceived, processed, and reasoned about by human beings, who may be biased or otherwise poor at data-driven decisions. Through the careful design of visualizations, we can attempt to address both perceptual and cognitive biases in how humans perceive and make use of data.</p>
-<p>This work attempts to use the power of visualization to improve human decision-making. I am particularly focused on presenting complex concepts like statistical uncertainty to general audiences in understandable and actionable ways. This work often involves creating novel designs that address known deficiencies in how people perceive data in visualizations.</p>
-<p>Example paper: <a href="http://graphics.cs.wisc.edu/Papers/2014/CG14/">Error Bars Considered Harmful: Exploring Alternate Encodings for Mean and Error</a></p>
 
-<li /><p><b>Visualizing Variation in Genomics Data:</b> New technologies and techniques in genomics are generating large amounts of data, affording new scales of inquiry. Techniques for visualizing this data must allow the viewer to build up a big picture of the data, while also being aware of uncertainty in how these data are collected and aggregated. The <a href="http://graphics.cs.wisc.edu/WP/layercake/">LayerCake</a> tool is an attempt to deploy a simple tool for visualizing multiple viral populations at once, find interesting regions of the viral genome, and drill down to areas of interest for closer analysis</p>
-<p>Example paper: <a href="http://graphics.cs.wisc.edu/Papers/2011/CGOG11/">Visualizing Virus Population Variability From Next Generation Sequencing Data</a></p>
+<li /><p><b>Ethical and "Black Hat" Visualization:</b> visualizations are sometimes our first, most impactful, or only interaction with data. This makes visualizations potentially very powerful. Wherever there is power, there is a potential for harm and misuse. I am interested in the ethical considations around visualization, both in terms of how specific visualizations can mislead or bias, but in an overall sense of designing best practices for visualization designers. This work involves reflections on the state of the field, case studies of specific systems, and empirical studies on visualization designs.</p>
+<p>Example papers:<br/>
+<a href="https://research.tableau.com/paper/ethical-dimensions-visualization-research">Ethical Dimensions of Visualization Research</a><br/>
+<a href="https://osf.io/gz98h/">Truncating the Y-Axis: Threat or Menace?</a>
+</p>
 
-<li /><p><b>Digital Humanities:</b> The humanities has benefitted from the new availability of data, but tools made for the visual analysis of data are often not very considerate of the different modes of proof, argumentation, and exploration that are the hallmarks of humanist research. By tailoring our tools to the specific rhetorics of our humanist collaborators, we can create visualizations which are data driven but still support human insight.</p>
+<li /><p><b>Visual Statistics:</b> If we know what someone wants from our data, then we can give them that information directly. In this case a database query might be more suitable than a visualization tool. But the promise of visualization lies in situations where the designer may not know exactly what the viewer wants to know, but the viewer is instead free to explore the dataset to build up understanding by themselves. In order for such designs to work, we as designers need to know how good users are at using <i>visual information</i> to build up big picture <i>statistical information</i> from data.</p>
+<p>Research from pyschology has shown that the visual system is capable of extracting a wide variety of statistical information (average values, variability, extrema) from visual stimuli in the world around us. My research in this area is an attempt to apply these findings to visualization, to show how we can design displays that rely on these abilities to let viewers build up "big pictures" by themselves.
+<p>Example papers: <br/>
+	<a href="https://github.com/AlgebraicVis/SanityCheck">Looka Good To Me: Visualizations as Sanity Checks</a><br/>
+	<a href="http://idl.cs.washington.edu/files/2017-RegressionByEye-CHI.pdf">Regression by Eye: Estimating Trends in Bivariate Visualizations</a>
+</p>
+
+<li /><p><b>Systems Building in Applied Domains:</b> the relationship that specific domains have with data is changing, as a result not just of "more" data, but <i>different</i> data. My expertise with uncertainty visualization, epistemology, and human computer interaction has led me to work with experts in diverse fields like the digitial humanities, genomics, and business intelligence to to solve data problems that have been overlooked by conventional visualization systems.</p>
+<p>Example papers: <br/>
+	<a href="http://graphics.cs.wisc.edu/Papers/2011/CWG11a/">Exploring Collections of Tagged Text for Literary Scholarship</a><br/>
+	<a href="http://graphics.cs.wisc.edu/Papers/2011/CGOG11/">Visualizing Virus Population Variability From Next Generation Sequencing Data</a>
+</p>
+
+<!--<li /><p><b>Visualizing Variation in Genomics Data:</b> New technologies and techniques in genomics are generating large amounts of data, affording new scales of inquiry. Techniques for visualizing this data must allow the viewer to build up a big picture of the data, while also being aware of uncertainty in how these data are collected and aggregated. The <a href="http://graphics.cs.wisc.edu/WP/layercake/">LayerCake</a> tool is an attempt to deploy a simple tool for visualizing multiple viral populations at once, find interesting regions of the viral genome, and drill down to areas of interest for closer analysis</p>
+<p>Example paper: <a href="http://graphics.cs.wisc.edu/Papers/2011/CGOG11/">Visualizing Virus Population Variability From Next Generation Sequencing Data</a></p>-->
+
+<!--<li /><p><b>Digital Humanities:</b> The humanities has benefitted from the new availability of data, but tools made for the visual analysis of data are often not very considerate of the different modes of proof, argumentation, and exploration that are the hallmarks of humanist research. By tailoring our tools to the specific rhetorics of our humanist collaborators, we can create visualizations which are data driven but still support human insight.</p>
 <p>Example paper: <a href="http://graphics.cs.wisc.edu/Papers/2011/CWG11a/">Exploring Collections of Tagged Text for Literary Scholarship</a></p>
-</ul>
+</ul>-->
 
 	</div>
 <?php
